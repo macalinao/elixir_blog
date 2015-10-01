398c20fe7a32b2aea521bf6f8ee910f0957a411c
Ian Macalinao <me@ian.pw>
2015-10-01 09:33:35 -0500

    User controller

--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,7 @@ defmodule ElixirBlog.Router do
   scope "/", ElixirBlog do
     pipe_through :browser # Use the default browser stack
 
+    resources "/users", UserController
     resources "/", PostController
   end
 
